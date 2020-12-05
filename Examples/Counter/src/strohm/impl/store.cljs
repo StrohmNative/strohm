e922c9cea7127e87dadab1caeee9b638c6e235f8
Stefan van den Oord <soord@mac.com>
2020-12-05 16:58:51 +0100

    Simplifications

--- Examples/Counter/src/strohm/impl/store.cljs
+++ Examples/Counter/src/strohm/impl/store.cljs
@@ -4,23 +4,10 @@
 
 (defonce global-store (atom nil))
 
-(defn state
-  [store]
-  (:state store))
-
-(defn reducer 
-  [store]
-  (:reducer store))
-
-(defn dispatcher
-  [store]
-  (:dispatcher store))
-
 (defn create-store 
   [reducer & {:keys [initial-state]}]
   {:state (or initial-state default-initial-state)
-   :reducer reducer
-   :dispatcher identity})
+   :reducer reducer})
 
 (defn reduce-action [store action] 
   (update store :state (partial (:reducer store) action)))
--- Examples/Counter/test/strohm/impl/store_test.cljs
+++ Examples/Counter/test/strohm/impl/store_test.cljs
@@ -1,9 +1,6 @@
 (ns strohm.impl.store-test
   (:require [cljs.test :refer [deftest testing is]]
             [strohm.impl.store :refer [create-store
-                                       state
-                                       reducer
-                                       dispatcher
                                        reduce-action]]))
 
 (defn identity-reducer [state _action] state)
@@ -11,16 +8,16 @@
 (deftest store-impl-test
   (testing "a store has a reducer, state and a dispatch function"
     (let [store (create-store 'some-reducer)]
-      (is (= {} (state store)))
-      (is (= 'some-reducer (reducer store)))
-      (is (fn? (dispatcher store)))))
+      (is (= {} (:state store)))
+      (is (= 'some-reducer (:reducer store)))))
 
   (testing "a store can be created with an initial state"
     (is (= {:test "foo"}
-           (state (create-store 'some-reducer
-                                :initial-state {:test "foo"})))))
-  
+           (:state (create-store 'some-reducer
+                                 :initial-state {:test "foo"})))))
+
   (testing "it can reduce an action to a new state"
-    (let [store (create-store (fn [_action state] (inc state))
-                              :initial-state 0)]
-      (is (= 1 (state (reduce-action store {:type :increment})))))))
+    (let [reducer (fn [_action state] (inc state))
+          store   (create-store reducer :initial-state 0)
+          action  {:type :increment}]
+      (is (= 1 (:state (reduce-action store action)))))))
