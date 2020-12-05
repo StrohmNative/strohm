56ed48c52fec0ace0f37f7f82c20af3081e933ca
Stefan van den Oord <soord@mac.com>
2020-12-05 20:25:00 +0100

    Basic API: `create-store` and `dispatch!`

--- Examples/Counter/src/strohm/store.cljs
+++ Examples/Counter/src/strohm/store.cljs
@@ -1,8 +1,17 @@
 (ns strohm.store
   (:require [strohm.impl.store :as impl]))
 
-(defonce global-store (atom nil))
+(defonce store (atom nil))
 
 (defn create-store
   [& args]
-  (reset! global-store (impl/create-store args)))
+  (reset! store (apply impl/create-store args)))
+
+(defn get-state
+  []
+  (:state @store))
+
+(defn dispatch!
+  [action]
+  (swap! store (partial impl/reduce-action action))
+  action)
--- Examples/Counter/test/strohm/store_test.cljs
+++ Examples/Counter/test/strohm/store_test.cljs
@@ -1,8 +1,19 @@
 (ns strohm.store-test
   (:require [cljs.test :refer [deftest is testing]]
-            [strohm.store :refer []]
-            [strohm.impl.store :as impl])
-  )
+            [strohm.store :refer [store create-store get-state dispatch!]]))
 
 (deftest store-test
-  )
+  (testing "store is nil initially"
+    (is (nil? @store)))
+
+  (testing "created store has initial state"
+    (reset! store nil)
+    (create-store {} :initial-state {:foo "bar"})
+    (is (= {:foo "bar"} (get-state))))
+  
+  (testing "dispatch an action"
+    (reset! store nil)
+    (create-store {:increment (fn incfn [state _] (inc state))}
+                  :initial-state 0)
+    (is (= {:type :increment} (dispatch! {:type :increment})))
+    (is (= 1 (get-state)))))
