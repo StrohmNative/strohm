1dce240ab44ed3deeda2af460c6b7868b3a90585
Stefan van den Oord <soord@mac.com>
2020-12-23 08:45:10 +0100

    Adds script handler for receiving messages from js

--- Examples/Counter/ios/Counter/Strohm.swift
+++ Examples/Counter/ios/Counter/Strohm.swift
@@ -15,10 +15,11 @@ struct WebViewWrapper: UIViewRepresentable {
     typealias UIViewType = WKWebView
 }
 
-class Strohm: NSObject, WKNavigationDelegate {
+class Strohm: NSObject, WKNavigationDelegate, WKScriptMessageHandler {
     static var `default` = Strohm()
 
     var webView: WKWebView?
+    var webConfiguration: WKWebViewConfiguration!
     var status: Status = .uninitialized
 
     static func determinePort(port: Int?, env: [String: String]) -> Int {
@@ -32,14 +33,25 @@ class Strohm: NSObject, WKNavigationDelegate {
     }
 
     public func install(appJsPath: String, port: Int? = nil) {
+        webConfiguration = WKWebViewConfiguration()
+        webConfiguration.userContentController.add(self, name: "jsToSwift")
+
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView?.navigationDelegate = self
+
         let port = Strohm.determinePort(port: port,
                                           env: ProcessInfo().environment)
-        webView = WKWebView()
-        webView?.navigationDelegate = self
         let myHtml = """
         <html>
         <body style='background-color: #ddd;font-size: 200%'>
-        <script src="http://localhost:\(port)/\(appJsPath)"></script>
+            <h1>Hi!</h1><div id='content'></div>
+            <script type="text/javascript">
+                window.onload = function(e) {
+                    document.getElementById('content').innerHTML += 'onload<br />'
+                    globalThis.app.main.init()
+                }
+            </script>
+            <script src="http://localhost:\(port)/\(appJsPath)"></script>
         </body>
         </html>
         """
@@ -57,6 +69,9 @@ class Strohm: NSObject, WKNavigationDelegate {
     }
 
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        webView.evaluateJavaScript("Object.getOwnPropertyNames(strohm.store)") { (result, error) in
+            print(result)
+        }
         webView.evaluateJavaScript("this.hasOwnProperty('strohm')") { (result, error) in
             guard let returnValue = result as? Int,
                 returnValue == 1,
@@ -89,6 +104,11 @@ class Strohm: NSObject, WKNavigationDelegate {
         } else {
             completionHandler(.performDefaultHandling, nil)
         }
+    }
+
+    func userContentController(_ userContentController: WKUserContentController,
+                               didReceive message: WKScriptMessage) {
+        print("jsToSwift", message.name, message.body)
     }
 
     enum Status {
