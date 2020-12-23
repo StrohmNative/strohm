92f017e421552830176b81c3b228785036276e18
Stefan van den Oord <soord@mac.com>
2020-12-23 08:46:46 +0100

    Uses message handler to post content to swift

--- Examples/Counter/src/app/main.cljs
+++ Examples/Counter/src/app/main.cljs
@@ -11,5 +11,20 @@
     (set! (.-innerHTML contentDiv) 
           (str oldHtml "<br/>\n[js] " msg))))
 
+(defn js->swift [msg]
+  (let [handler  (->> js/window
+                      (.-webkit)
+                      (.-messageHandlers)
+                      (.-jsToSwift))]
+    (.postMessage handler msg)))
+
+(defn sendTestMessage []
+  (log "send test message")
+  (js->swift "{\"foo\":\"bar\"}"))
+
+(defn init []
+  (js/setTimeout sendTestMessage 10000)
+  (log "init done"))
+
 (defn ^:dev/after-load reload! []
   (println "[main] reloaded"))
