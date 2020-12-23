abfbd04498fd30983f2778099c768af51bca7995
Stefan van den Oord <soord@mac.com>
2020-12-23 08:46:25 +0100

    Adds fn to append log content from cljs

--- Examples/Counter/src/app/main.cljs
+++ Examples/Counter/src/app/main.cljs
@@ -5,5 +5,11 @@
   (create-store (fn [_action state] state))
   (println "[main] started"))
 
+(defn log [msg]
+  (let [contentDiv (.getElementById js/document "content")
+        oldHtml    (.-innerHTML contentDiv)]
+    (set! (.-innerHTML contentDiv) 
+          (str oldHtml "<br/>\n[js] " msg))))
+
 (defn ^:dev/after-load reload! []
   (println "[main] reloaded"))
