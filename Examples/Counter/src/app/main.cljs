8905f93cd0ca78bfdb5537b502037f492e9bf795
Stefan van den Oord <soord@mac.com>
2020-12-23 09:02:05 +0100

    Logging enhancements

--- Examples/Counter/src/app/main.cljs
+++ Examples/Counter/src/app/main.cljs
@@ -1,15 +1,21 @@
 (ns app.main
-  (:require [strohm.store :refer [create-store]]))
+  (:require [strohm.store :refer [create-store]]
+            [clojure.string :as str]))
 
 (defn main! []
   (create-store (fn [_action state] state))
   (println "[main] started"))
 
-(defn log [msg]
+(defn log [& args]
   (let [contentDiv (.getElementById js/document "content")
         oldHtml    (.-innerHTML contentDiv)]
     (set! (.-innerHTML contentDiv) 
-          (str oldHtml "<br/>\n[js] " msg))))
+          (str oldHtml "<br/>\n[js] " (str/join " " args)))))
+
+(defn clear-log []
+  (let [contentDiv (.getElementById js/document "content")]
+    (set! (.-innerHTML contentDiv) "")))
+(comment (clear-log))
 
 (defn js->swift [msg]
   (let [handler  (->> js/window
@@ -19,11 +25,12 @@
     (.postMessage handler msg)))
 
 (defn sendTestMessage []
-  (log "send test message")
+  (log "send test message"
+       (str/join ":" ((juxt #(.getHours %) #(.getMinutes %) #(.getSeconds %)) (js/Date.))))
   (js->swift "{\"foo\":\"bar\"}"))
 
 (defn init []
-  (js/setTimeout sendTestMessage 10000)
+  (js/setTimeout sendTestMessage 3000)
   (log "init done"))
 
 (defn ^:dev/after-load reload! []
