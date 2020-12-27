6d41eca5593946845b7123039769f1c4cb2c68f9
Stefan van den Oord <soord@mac.com>
2020-12-27 15:05:05 +0100

    Adds Swift UI for counter

--- Examples/Counter/ios/Counter/ContentView.swift
+++ Examples/Counter/ios/Counter/ContentView.swift
@@ -1,16 +1,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count: Int = 0
+    let countFormatter = NumberFormatter()
+
     var body: some View {
         VStack {
+            Text("Hello, world!")
+                .padding()
             HStack {
-                Text("Hello, world!")
+                Spacer()
+                Button(action: self.decrement, label: { Text("–") })
                     .padding()
-                Button(action: { Strohm.default.reload() },
-                       label: { Text("Reload") })
-            }
+
+                TextField("", value: $count, formatter: countFormatter)
+                    { _ in }
+                    onCommit: { self.setCounter(count: count) }
+                    .scaledToFit()
+                    .frame(minWidth: 70)
+                    .padding()
+                Button(action: self.increment, label: { Text("+") })
+                    .padding()
+                Spacer()
+            }.padding()
+            Button(action: { Strohm.default.reload() }, label: { Text("Reload") })
+                .padding()
             StrohmHolder()
         }
+    }
+
+    func decrement() {
+        print("–")
+    }
+
+    func increment() {
+        print("+")
+    }
+
+    func setCounter(count: Int) {
+        print("setCounter", count)
     }
 }
 
