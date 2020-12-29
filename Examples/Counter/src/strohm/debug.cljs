b05c38acc9f63d6e6f1f37630a782ff0e3f786aa
Stefan van den Oord <soord@mac.com>
2020-12-29 14:55:14 +0100

    Log to console when no document available

--- Examples/Counter/src/strohm/debug.cljs
+++ Examples/Counter/src/strohm/debug.cljs
@@ -2,10 +2,12 @@
   (:require [clojure.string :as str]))
 
 (defn log [& args]
-  (let [contentDiv (.getElementById js/document "content")
-        oldHtml    (.-innerHTML contentDiv)]
-    (set! (.-innerHTML contentDiv)
-          (str oldHtml "<br/>\n[js] " (str/join " " args)))))
+  (if-let [document js/document]
+    (let [contentDiv (.getElementById document "content")
+          oldHtml    (.-innerHTML contentDiv)]
+      (set! (.-innerHTML contentDiv)
+            (str oldHtml "<br/>\n[js] " (str/join " " args))))
+    (apply js/console.debug args)))
 
 (defn clear-log []
   (let [contentDiv (.getElementById js/document "content")]
