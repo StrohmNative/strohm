39398942fde519090534ca94a97818803c18686b
Stefan van den Oord <soord@mac.com>
2020-12-29 08:27:45 +0100

    Defines the counter reducer

--- Examples/Counter/src/app/main.cljs
+++ Examples/Counter/src/app/main.cljs
@@ -1,23 +1,32 @@
 (ns app.main
   (:require [strohm.store :refer [create-store]]
             [strohm.tx :refer [js->native]]
-            [strohm.debug :refer [log]]
+            [strohm.debug :as debug]
             [clojure.string :as str]))
 
+(defn reducer 
+  [state action]
+  (debug/log "reduce:" action (:type action) ((comp :count :payload) action))
+  (case (:type action)
+    "decrement" (dec state)
+    "increment" (inc state)
+    "setCounter" ((comp :count :payload) action)
+    state))
+
 (defn ^:export main! []
-  (create-store (fn [_action state] state))
+  (create-store reducer :initial-state 0)
   (println "[main] started"))
 
 (defn sendTestMessage []
   (let [time ((juxt #(.getHours %) #(.getMinutes %) #(.getSeconds %)) (js/Date.))]
-    (log "send test message"
+    (debug/log "send test message"
          (str/join ":" time))
     (js->native {:foo :bar
                  :when time})))
 
 (defn ^:export init []
   (js/setTimeout sendTestMessage 3000)
-  (log "init done"))
+  (debug/log "init done"))
 
 (defn ^:dev/after-load reload! []
   (println "[main] reloaded"))
