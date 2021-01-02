50925b32e40663765f1f83a9a4abfba247760a73
Stefan van den Oord <soord@mac.com>
2021-01-02 09:38:28 +0100

    [android] Calls dispatch on the various UI actions

--- Examples/Counter/android/app/src/main/java/com/vandenoord/counter/MainActivity.kt
+++ Examples/Counter/android/app/src/main/java/com/vandenoord/counter/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.vandenoord.counter
 
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import dev.strohmNative.Strohm
 
@@ -9,9 +15,52 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val txtCounter = findViewById<EditText>(R.id.txtCounter)
+        txtCounter.setOnEditorActionListener(object : TextView.OnEditorActionListener {
+            override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
+                return onEnterCount(v)
+            }
+        })
     }
 
-    fun reload(src: android.view.View) {
+    fun decrement(src: View) {
+        Strohm.getInstance().dispatch("decrement")
+    }
+
+    fun increment(src: View) {
+        Strohm.getInstance().dispatch("increment")
+    }
+
+    fun setCounter(count: Int) {
+        Strohm.getInstance().dispatch("setCounter", mapOf("count" to count))
+    }
+
+    fun reload(src: View) {
         Strohm.getInstance().reload()
+    }
+
+    fun subscribe(src: View) {
+    }
+
+    fun unsubscribe(src: View) {
+
+    }
+
+    private fun onEnterCount(v: TextView?): Boolean {
+        Log.d("app", "onEditorAction")
+        hideSoftKeyBoard()
+        v?.clearFocus()
+        if (v?.text != null) {
+            val count = Integer.valueOf(v.text as String)
+            setCounter(count.toInt())
+        }
+        return true
+    }
+
+    private fun hideSoftKeyBoard() {
+        val imm: InputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        if (imm.isAcceptingText) {
+            imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+        }
     }
 }
