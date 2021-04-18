bad5c4cd097e8d00699e52f66db2caffc8647905
Stefan van den Oord <soord@mac.com>
2021-04-18 16:23:26 +0200

    [examples] Uses the new way of integrating Strohm

--- Examples/Journal/ios/Journal/JournalApp.swift
+++ Examples/Journal/ios/Journal/JournalApp.swift
@@ -3,13 +3,11 @@ import Strohm
 
 @main
 struct JournalApp: App {
+    let strohm = Strohm.default
+
     var body: some Scene {
         WindowGroup {
             ContentView()
-            StrohmHolder()
-                .frame(minWidth: 0, idealWidth: 0, maxWidth: 0,
-                       minHeight: 0, idealHeight: 0, maxHeight: 0,
-                       alignment: .center)
         }
     }
 }
