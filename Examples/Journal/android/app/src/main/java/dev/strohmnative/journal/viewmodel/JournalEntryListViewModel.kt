f619abc839a4b8c8f66f01bb63a3056d0c6fa6b6
Stefan van den Oord <soord@mac.com>
2021-04-20 21:40:02 +0200

    [examples] Adds ability to sort data

--- Examples/Journal/android/app/src/main/java/dev/strohmnative/journal/viewmodel/JournalEntryListViewModel.kt
+++ Examples/Journal/android/app/src/main/java/dev/strohmnative/journal/viewmodel/JournalEntryListViewModel.kt
@@ -12,11 +12,13 @@ import dev.strohmnative.strohm.PropPath
 import dev.strohmnative.strohm.Props
 import dev.strohmnative.strohm.Strohm
 import java.util.*
+import kotlin.Comparator
 
 
 class JournalEntryListViewModel: BaseObservable {
     @Bindable
     var entries: List<JournalEntry>
+    var sorter: Comparator<JournalEntry>? = null
 
     private var propName: PropName
     private var propPath: PropPath
@@ -27,6 +29,9 @@ class JournalEntryListViewModel: BaseObservable {
         this.entries = entries
         this.propName = "entries"
         this.propPath = listOf("entries")
+        this.sorter = Comparator<JournalEntry>{ e1, e2 ->
+            - e1.created.compareTo(e2.created)
+        }
         Strohm.getInstance().subscribe(mapOf(propName to propPath), ::receiveProps) {
             subscriptionId -> this.subscriptionId = subscriptionId
         }
@@ -44,8 +49,8 @@ class JournalEntryListViewModel: BaseObservable {
     fun propsToData(props: Props): List<JournalEntry>? {
         val m = props[this.propName] as? Map<*, *> ?: return null
         val rawData = m.asMapOfType<PropName, Props>() ?: return null
-        val data = rawData.values.mapNotNull(JournalEntry::createFromDict)
-        return data
+        var data = rawData.values.mapNotNull(JournalEntry::createFromDict)
+        return sorter?.let { data.sortedWith(it) } ?: data
     }
 }
 
