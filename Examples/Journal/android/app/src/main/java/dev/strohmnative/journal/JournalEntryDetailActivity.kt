f5cb0aa737e1bda851e67a867867a1633c284546
Stefan van den Oord <soord@mac.com>
2021-05-04 16:30:23 +0200

    [examples] Adds ability to change title

--- Examples/Journal/android/app/src/main/java/dev/strohmnative/journal/JournalEntryDetailActivity.kt
+++ Examples/Journal/android/app/src/main/java/dev/strohmnative/journal/JournalEntryDetailActivity.kt
@@ -2,15 +2,16 @@ package dev.strohmnative.journal
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.MenuItem
+import android.view.WindowManager
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import dev.strohmnative.journal.databinding.ActivityJournalEntryDetailBinding
 import dev.strohmnative.journal.model.JournalEntry
 import dev.strohmnative.journal.viewmodel.JournalEntryDetailViewModel
+import dev.strohmnative.strohm.Strohm
 
 /**
  * An activity representing a single JournalEntry detail screen. This
@@ -28,9 +29,30 @@ class JournalEntryDetailActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.detailToolbar)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own detail action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        binding.fab.setOnClickListener { _ ->
+            val dialogView = layoutInflater.inflate(R.layout.change_title_dialog, null)
+            val editField = dialogView.findViewById<EditText>(R.id.editTitle)
+            binding.viewModel?.data?.value?.title?.let { editField.setText(it) }
+
+            val alert = AlertDialog.Builder(this)
+            alert.setView(dialogView)
+            alert.setTitle("Change Title")
+
+            alert.setPositiveButton("Update") { _, _ ->
+                binding.viewModel?.data?.let { data ->
+                    data.value = data.value?.let { entry ->
+                        entry.title = editField.text.toString()
+                        Strohm.getInstance().dispatch("update-entry", entry)
+                        entry
+                    }
+                }
+            }
+            alert.setNegativeButton("Cancel") { _, _ -> }
+            val d = alert.create()
+            editField.requestFocus()
+            d.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
+            editField.setSelection(editField.text.length)
+            d.show()
         }
 
         // Show the Up button in the action bar.
