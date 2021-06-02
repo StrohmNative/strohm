12229d7a448d8f6757e9cf4bc3d90150d63f50fd
Stefan van den Oord <soord@mac.com>
2021-06-02 11:40:46 +0200

    [common][wip] this works; not as pure as it can be?!

--- Strohm/src/strohm/impl/store.cljs
+++ Strohm/src/strohm/impl/store.cljs
@@ -43,3 +43,44 @@
 
 (defn state->props [state props-spec]
   (into {} (map (partial state-for-prop-spec state) props-spec)))
+
+(comment
+  ;;
+  ;; USING STORE AS ATOM
+  ;;
+  (let [store (atom nil)
+        reduce-action' (fn [s action]
+                         (let [reducer     (:reducer s)
+                               reducing-fn (get-reducer-fn reducer (:type action))
+                               updated-s (update s
+                                                 :state
+                                                 (fn [state] (tap> state) (reducing-fn state action)))]
+                           (tap> updated-s)
+                           updated-s))
+        create-store' (fn [reducer & {:keys [initial-state middlewares]}]
+                        (let [base-dispatch (fn base-dispatch [action] (swap! store reduce-action' action))]
+                          {:state (or initial-state default-initial-state)
+                           :reducer reducer
+                           :dispatch ((apply comp (map (fn [m] (m store)) (reverse middlewares))) base-dispatch)}))
+        reducer (fn [state action]
+                  (update state :received-actions #(conj % action)))
+        logger-middleware (fn [store]
+                            (fn logger-middleware [next]
+                              (fn logger-dispatcher [action]
+                                (prn "state before:" (:state @store))
+                                (prn "dispatch action:" action)
+                                (next action)
+                                (prn "state after:" (:state @store))
+                                (prn "------------------------"))))
+        action-duplicating-middleware (fn [_store]
+                                        (fn action-duplicating-middleware [next]
+                                          (fn action-duplicating-dispatcher [action]
+                                            (next action)
+                                            (next action))))
+        dispatch          (fn [a] ((:dispatch @store) a))]
+    (reset! store (create-store' reducer
+                                 :initial-state {:received-actions []}
+                                 :middlewares [logger-middleware action-duplicating-middleware]))
+    (dispatch {:type "first action"})
+    (dispatch {:type "second action"})
+    @store))
