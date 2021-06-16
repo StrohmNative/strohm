861b4c4c9469d5a8884e53bf066bc657dfbba733
Stefan van den Oord <soord@mac.com>
2021-06-16 11:39:39 +0200

    [common] Refactor: extract some functions

--- Strohm/src/strohm/tx.cljs
+++ Strohm/src/strohm/tx.cljs
@@ -15,20 +15,25 @@
   []
   (some? (.-webkit js/window)))
 
+(defn props->message [subscriptionId old-props new-props]
+  {:function "subscriptionUpdate"
+   :subscriptionId (str subscriptionId)
+   :old old-props
+   :new new-props})
+
+(defn send-message [message]
+  (cond
+    (android-webview?)
+    (.receiveProps (.-strohmReceiveProps js/globalThis)
+                   (js/JSON.stringify (clj->js' message)))
+
+    (ios-webview?)
+    (.postMessage (js->swift-handler)
+                  (clj->js' message))
+
+    :else
+    (js/console.error "Neither Android nor iOS callback interface was found.")))
+
 (defn send-props
   [subscriptionId old-props new-props]
-  (let [message {:function "subscriptionUpdate"
-                 :subscriptionId (str subscriptionId)
-                 :old old-props
-                 :new new-props}]
-    (cond
-      (android-webview?)
-      (.receiveProps (.-strohmReceiveProps js/globalThis)
-                     (js/JSON.stringify (clj->js' message)))
-
-      (ios-webview?)
-      (.postMessage (js->swift-handler)
-                    (clj->js' message))
-
-      :else
-      (js/console.error "Neither Android nor iOS callback interface was found."))))
+  (send-message (props->message subscriptionId old-props new-props)))
