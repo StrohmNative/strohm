4b47f928395b9241c75e38e40291d1bf92bbde67
Stefan van den Oord <soord@mac.com>
2021-06-25 22:07:47 +0200

    [examples] Adds middleware for persisting state

--- Examples/Journal/src/app/main.cljs
+++ Examples/Journal/src/app/main.cljs
@@ -1,16 +1,32 @@
 (ns app.main
-  (:require [strohm.native :refer [create-store combine-reducers]]
+  (:require [strohm.native :refer [create-store combine-reducers clj->js']]
             [app.entries.reducer :as entries]
-            [app.navigation.reducer :as navigation]))
+            [app.navigation.reducer :as navigation]
+            [strohm.tx :as tx]
+            [strohm.debug :as debug]))
 
 (def reducer (combine-reducers {"entries" entries/reducer}))
+
+(defn send-state! [state]
+  (let [serialized-state (js/JSON.stringify (clj->js' state))]
+    (tx/send-message! {:function "persistState"
+                       :state serialized-state})))
+
+(defn persist-state-middleware
+  [next]
+  (fn [store action]
+    (let [next-store (next store action)]
+      (send-state! (:state next-store))
+      next-store)))
 
 (def empty-store
   {"entries" entries/initial-state
    :navigation navigation/initial-state})
 
 (defn- setup []
-  (create-store reducer :initial-state empty-store))
+  (create-store reducer
+                :initial-state empty-store
+                :middlewares [persist-state-middleware]))
 
 (defn ^:export main! []
   (debug/set-logging-enabled!)
