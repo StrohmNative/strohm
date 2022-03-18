1c399ce9e39c67ce9d7212902d44d7fab5a4fbd8
Stefan van den Oord <soord@mac.com>
2022-03-18 22:03:08 +0100

    [common] Remove unused spec

--- Strohm/src/strohm_native/spec.cljs
+++ Strohm/src/strohm_native/spec.cljs
@@ -10,7 +10,6 @@
 (>def :strohm/prop-path (s/coll-of (s/or :keyword keyword? :index nat-int?) :kind vector?))
 (>def :strohm/prop-spec (s/tuple :strohm/prop-name :strohm/prop-path))
 (>def :strohm/prop-value (s/tuple :strohm/prop-name any?))
-(>def :strohm/props (s/keys))
 (>def :strohm/reducer-fn (s/with-gen ifn? #(s/gen #{(fn [state _action] state)})))
 (>def :strohm/reducer (s/or :fn :strohm/reducer-fn :map map?))
 (>def :strohm/dispatch (s/with-gen ifn? #(s/gen #{(fn [store _action] store)})))
