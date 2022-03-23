0d4c257e47766ab27e49a44f2d517d2149888142
Stefan van den Oord <soord@mac.com>
2022-03-23 09:19:23 +0100

    [common] Fix specs

--- Strohm/src/strohm_native/spec.cljs
+++ Strohm/src/strohm_native/spec.cljs
@@ -5,18 +5,26 @@
             [clojure.test.check.properties]
             [com.fulcrologic.guardrails.core :refer [>def ? >fspec]]))
 
-(>def :strohm/prop-name string?)
+(>def :strohm/prop-name (s/or :keyword keyword?
+                              :string string?))
+
 (>def :strohm/state any?)
-(>def :strohm/prop-path (s/coll-of (s/or :keyword keyword? :index nat-int?) :kind vector?))
+
+(>def :strohm/prop-path
+  (s/coll-of (s/or :keyword keyword?
+                   :index nat-int?
+                   :string string?)
+             :kind vector?))
+
 (>def :strohm/prop-spec (s/tuple :strohm/prop-name :strohm/prop-path))
 (>def :strohm/prop-value (s/tuple :strohm/prop-name any?))
 (>def :strohm/reducer-fn (s/with-gen ifn? #(s/gen #{(fn [state _action] state)})))
 (>def :strohm/reducer (s/or :fn :strohm/reducer-fn :map map?))
 (>def :strohm/dispatch (s/with-gen ifn? #(s/gen #{(fn [store _action] store)})))
 (>def :strohm/store (s/keys :opt-un [:strohm/state :strohm/reducer :strohm/dispatch]))
-(s/def ::store-action-fn :strohm/dispatch)
+(>def ::store-action-fn :strohm/dispatch)
 
-(s/def :strohm/middleware
+(>def :strohm/middleware
   (s/fspec :args (s/cat :next :strohm/dispatch)
            :ret ::store-action-fn))
 
